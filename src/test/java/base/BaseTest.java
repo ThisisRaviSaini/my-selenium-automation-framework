00da06efcdf2426234de0613c076a0ffa8127328
Ravi Saini <thisisravisaini@gmail.com>
2025-10-02 10:57:16 +0530

    Added Driver Factory Class

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,4 +2,6 @@ package base;
 
 public class BaseTest {
 
+
+    
 }
---
+++ src/test/java/utilities/DriverFactory.java
@@ -0,0 +1,38 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverFactory {
+
+    public static WebDriver driver;
+
+    public static WebDriver initDriver(String browser) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else {
+            throw new IllegalArgumentException("Browser not supported: " + browser);
+        }
+        driver.manage().window().maximize();
+        return driver;
+    }
+
+    public static WebDriver getDriver() {
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
+
+
