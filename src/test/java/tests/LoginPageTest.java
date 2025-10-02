9ce7574d1a51fcce375f1dbab49445138d11d456
Ravi Saini <thisisravisaini@gmail.com>
2025-10-02 14:30:10 +0530

    Added Assertion

--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -1,26 +1,30 @@
 package tests;
 
 import base.BaseTest;
-import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pageobjectmodels.LoginPage;
 import utilities.ExcelFileReader;
 
 
-
 public class LoginPageTest extends BaseTest {
+
     ExcelFileReader excelFileReader = new ExcelFileReader();
     LoginPage loginPage = new LoginPage(driver);
+    SoftAssert softAssert = new SoftAssert();
 
     @Test
     public void successLoginTest() {
 
         String username = excelFileReader.getData("TC_01", "Username");
         String password = excelFileReader.getData("TC_01", "Password");
-        System.out.println("Username: "+username);
-        System.out.println("Password: "+password);
 
-        loginPage.performLogin(username,password);
-        
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+
+        loginPage.performLogin(username, password);
+
+        softAssert.assertEquals("ABC", "ABC");
+        softAssert.assertAll();
     }
 }
