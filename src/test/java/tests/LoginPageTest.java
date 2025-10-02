f379d5cc8c6be2f8f3a01d6fc277106e2d429792
Ravi Saini <thisisravisaini@gmail.com>
2025-10-02 14:28:12 +0530

    Changes

--- src/test/java/tests/LoginPageTest.java
+++ src/test/java/tests/LoginPageTest.java
@@ -9,19 +9,18 @@ import utilities.ExcelFileReader;
 
 
 public class LoginPageTest extends BaseTest {
-
+    ExcelFileReader excelFileReader = new ExcelFileReader();
+    LoginPage loginPage = new LoginPage(driver);
 
     @Test
     public void successLoginTest() {
-        ExcelFileReader excelFileReader = new ExcelFileReader();
-        LoginPage loginPage = new LoginPage(driver);
+
         String username = excelFileReader.getData("TC_01", "Username");
         String password = excelFileReader.getData("TC_01", "Password");
         System.out.println("Username: "+username);
         System.out.println("Password: "+password);
 
         loginPage.performLogin(username,password);
-
-
+        
     }
 }
